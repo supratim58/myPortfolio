ec1575b41f24ba734ecd3d73550a628ed30f0288
Supratim Haldar <149317153+supratim58@users.noreply.github.com>
2024-10-11 22:49:13 +0530

    Starting..
    Upload the files. Next time when ever I make some changes there, I commit it.

---
+++ commands.js
@@ -0,0 +1,77 @@
+//var youtube = "#";
+//var twitter = "#";
+var password = "sonu";
+var linkedin = "https://www.linkedin.com/in/supratim-haldar/";
+var instagram = "https://www.instagram.com/supratim_haldar001/";
+var github = "https://github.com/supratim58/";
+var email = 'mailto:supratimhaldar58@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Supratim!👋",
+  "I'm a Engineering student, who builds engaging websites like this one",
+  "Enthusiustic in Cyber Security and Machine Learning.",
+  "Currently pursuing my graduation Btech in Computer Science and Engineering.",
+  "I learn the fundumentals of web development","Using it I build this website.",
+  "Then I started learning ML & cyber security fundamentals.",
+  "Gaining knowledge about networks, linux, virtual machine etc,",
+  "also numpy, pandas, keras, tenserflow etc.",
+  "Currently woring on cyber security practicals from \"LetsDefends\"",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of \"Who am I?\" is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  //'youtube        <a href="' + youtube + '" target="_blank"> ' + "</a>",
+  //'twitter        <a href="' + twitter + '" target="_blank"> ' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/supratim-haldar' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/supratim_haldar001' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/supratim58' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Supratim?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+
+banner = [
+    "                                                                                      ",
+    "    _____                        __  _                __  __      __    __          ",
+    "   / ___/__  ______  _________ _/ /_(_)___ ___       / / / /___ _/ /___/ /___ ______",
+    "   \\__ \\/ / / / __ \\/ ___/ __ `/ __/ / __ `__ \\     / /_/ / __ `/ / __  / __ `/ ___/",
+    "  ___/ / /_/ / /_/ / /  / /_/ / /_/ / / / / / /    / __  / /_/ / / /_/ / /_/ / /    ",
+    "  ____/\\__,_/ .___/_/   \\__,_/\\__/_/_/ /_/ /_/    /_/ /_/\\__,_/_/\\__,_/\\__,_/_/     ",
+    "           /_/                                                        portfolio@2024     ",
+    '<span class="color2">Welcome to my interactive web terminal portfolio😀.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  ];
